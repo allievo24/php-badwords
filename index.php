fcec1ebd69b5de946855856fcf55ab25a0eb5800
saidmoussadek <saiddus24@gmail.com>
2022-09-12 16:32:19 +0200

    push

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <body>
         
     
-       <form action="index.php" method="post">
+       <form action="ricevi_dati.php" method="post">
         <input type="text" name="parolaccia" placeholder="Inserici parola"/>
         <button type="submit">Invia</button>
        </form> 
